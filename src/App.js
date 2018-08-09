04393fcc8a5fe64e18c4d7b55bfc3f61452c1726
Luis Avila <luis.avila@globant.com>
2018-08-09 17:55:34 -0500

    Doing the Base Syntax Assignment

--- src/App.js
+++ src/App.js
@@ -1,18 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Person from './Person/Person';
+import Useroutput from './User/UserOutput';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: "Luis", age: 33},
+      {name: "Lalo", age: 34},
+      {name: "Cristi", age: 43}
+    ]
+  }
+
+  switchNameHandler = (newName) => {
+    //console.log("Was clicked!");
+    //DONT DO THIS!!: this.state.persons[0].name = "Luis";
+    this.setState({
+      persons: [
+        {name: newName, age: 33},
+        {name: "Lalo", age: 34},
+        {name: "O hell no", age: 3}
+      ]
+    })
+  }
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        {name: "Luis", age: 33},
+        {name: event.target.value, age: 34},
+        {name: "O hell no", age: 3}
+      ]
+    })
+  }
   render() {
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: " 1px solid blue",
+      padding: "8px",
+      cursor: "pointer"
+    };
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1>Well Hello there!</h1>
+      <p>Hi there whats yo namee!</p>
+      <button
+        style={style}
+        onClick ={() => this.switchNameHandler("Josey")}>Switch Name</button>
+      <Person
+      name={this.state.persons[0].name}
+      age={this.state.persons[0].age} />
+      <Person
+      name={this.state.persons[1].name}
+      age={this.state.persons[1].age}
+      click={this.switchNameHandler.bind(this, "Felicia")}
+      changed={this.nameChangedHandler}>My Hobbies are to knock you out</Person>
+      <Person
+      name={this.state.persons[2].name}
+      age={this.state.persons[2].age} />
+      <Useroutput />
       </div>
     );
   }
---
+++ src/User/UserOutput.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const userOutput = (props) => {
+  return (
+  <div className="UserOutput">
+    <p>Hopefully this works!</p>
+    <p>Yeah you sho right son!</p>
+  </div>
+)};
+
+export default userOutput;
